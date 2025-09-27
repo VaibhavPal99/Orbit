f2a2eb4d0fc7ac7ce0f0401591e8b547c616f609
VaibhavPal99 <vaibhavpal9935@gmail.com>
2025-09-27 13:33:56 +0530

    added cors

--- chat-server/src/index.ts
+++ chat-server/src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import http from "http";
+import cors from "cors"
 import { websocketSetup } from "./controller/message.controller.js";
 import messageRoutes from "./routes/message.routes.js";
 
 const app = express();
 const httpServer = http.createServer(app);
+
+
+app.use(cors({
+    origin: ["http://localhost:3000", "https://orbit-blond.vercel.app/"], // frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 
 websocketSetup(httpServer);
